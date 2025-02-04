c6634bdf6af05c62bdbc88da9cddcaf49e60c9a7
carolusquintus <carloschars@gmail.com>
2025-02-04 14:01:14 -0600

    Recursion with factorial() function excercise

---
+++ l4/recursion.hs
@@ -0,0 +1,16 @@
+-- RECURSION
+
+-- Function calls itself
+-- FACTORIAL
+-- factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+
+factorial :: Int -> Int
+-- base case
+factorial 0 = 1
+-- recursive case
+factorial n = n * factorial(n - 1)
+
+main = do
+    putStrLn "Factorial of your number is: "
+    print(factorial(5))
+
